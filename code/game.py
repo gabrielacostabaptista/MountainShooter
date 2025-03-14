5de9e1d1e74c7209943cb59300999542978674ba
GabrielaCB <gabrielacbap@gmail.com>
2025-03-13 22:14:39 -0300

    adjusting menu: image and window

--- code/game.py
+++ code/game.py
@@ -2,25 +2,35 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
+        # Inicializa o Pygame
         pygame.init()
-        self.window = pygame.display.set_mode((600, 480))  # Configura a janela do jogo
+
+        # Configura a janela
+        self.window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))  # Dimensões da janela
+        pygame.display.set_caption("Meu Jogo com Menu")  # Título da janela
+
+        # Controle de execução
+        self.running = True
+
+        # Instancia o menu
+        self.menu = Menu(self.window)
 
     def run(self):
-        while True: #chamar o MENU
-            menu = Menu(self.window)
-            menu.run()
-            pass
 
+        while self.running:  # Loop principal
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:  # Evento de sair
+                    self.running = False
 
-            #for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:  # fechando a janela
-            #       running = False
+            # Atualiza o menu
+            self.menu.run()
 
-        #pygame.quit()  # encerrando o pygame
-
+        # Encerra o Pygame
+        pygame.quit()
 
